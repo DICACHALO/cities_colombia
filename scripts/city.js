7c8940d5e7178e70b3aaadd419a9be4cefd4ca6e
Diana Chacon <dchacon871@unab.edu.co>
2021-07-13 12:33:34 -0500

    add links about cities

--- scripts/city.js
+++ scripts/city.js
@@ -6,103 +6,105 @@ let capital = document.getElementById("capital");
 let message = "La capital de " + departamento + " es: ";
 let message2 = " Da clic aquí para más información: "
 
+
+
 if (departamento ==="amazonas") {
-    capital.innerHTML = message + " Leticia." + message2 + "<a href='#'> Leticia </a>";
+    capital.innerHTML = message + " Leticia." + message2 + "<a href='http://www.leticia-amazonas.gov.co/'> Leticia </a>";
 }
-if (departamento ==="antioquia") {
-    capital.innerHTML = message + " Medellín." + message2 + "<a href='#'> Medellín </a>";
+else if (departamento ==="antioquia") {
+    capital.innerHTML = message + " Medellín." + message2 + "<a href='https://www.medellin.gov.co/irj/portal/medellin'> Medellín </a>";
 }
-if (departamento ==="arauca") {
-    capital.innerHTML = message + " Arauca." + message2 + "<a href='#'> Arauca </a>";
+else if (departamento ==="arauca") {
+    capital.innerHTML = message + " Arauca." + message2 + "<a href='https://www.arauca-arauca.gov.co/Paginas/default.aspx'> Arauca </a>";
 }
-if (departamento ==="atlantico") {
-    capital.innerHTML = message + " Barranquilla." + message2 + "<a href='#'> Barranquilla </a>";
+else if (departamento ==="atlantico") {
+    capital.innerHTML = message + " Barranquilla." + message2 + "<a href='https://www.barranquilla.gov.co/'> Barranquilla </a>";
 }
-if (departamento ==="bolivar") {
-    capital.innerHTML = message + " Cartagena de Indias." + message2 + "<a href='#'> Cartagena de Indias </a>";
+else if (departamento ==="bolivar") {
+    capital.innerHTML = message + " Cartagena de Indias." + message2 + "<a href='https://www.cartagena.gov.co/'> Cartagena de Indias </a>";
 }
-if (departamento ==="boyaca") {
-    capital.innerHTML = message + " Tunja." + message2 + "<a href='#'> Tunja </a>";
+else if (departamento ==="boyaca") {
+    capital.innerHTML = message + " Tunja." + message2 + "<a href='https://www.tunja-boyaca.gov.co/'> Tunja </a>";
 }
-if (departamento ==="caldas") {
-    capital.innerHTML = message + " Manizales." + message2 + "<a href='#'> Manizalez </a>";
+else if (departamento ==="caldas") {
+    capital.innerHTML = message + " Manizales." + message2 + "<a href='https://manizales.gov.co/'> Manizalez </a>";
 }
-if (departamento ==="caqueta") {
-    capital.innerHTML = message + " Florencia." + message2 + "<a href='#'> Florencia </a>";
+else if (departamento ==="caqueta") {
+    capital.innerHTML = message + " Florencia." + message2 + "<a href='http://www.florencia-caqueta.gov.co/'> Florencia </a>";
 }
-if (departamento ==="casanare") {
-    capital.innerHTML = message + " Yopal." + message2 + "<a href='#'> Yopal </a>";
+else if (departamento ==="casanare") {
+    capital.innerHTML = message + " Yopal." + message2 + "<a href='http://www.yopal-casanare.gov.co/'> Yopal </a>";
 }
-if (departamento ==="cauca") {
-    capital.innerHTML = message + " Popayán." + message2 + "<a href='#'> Popayán </a>";
+else if (departamento ==="cauca") {
+    capital.innerHTML = message + " Popayán." + message2 + "<a href='http://www.popayan.gov.co/'> Popayán </a>";
 }
-if (departamento ==="cesar") {
-    capital.innerHTML = message + " Valledupar." + message2 + "<a href='#'> Valledupar </a>";
+else if (departamento ==="cesar") {
+    capital.innerHTML = message + " Valledupar." + message2 + "<a href='http://valledupar-cesar.gov.co/Paginas/default.aspx'> Valledupar </a>";
 }
-if (departamento ==="choco") {
-    capital.innerHTML = message + " Quibdó." + message2 + "<a href='#'> Quibdó </a>";
+else if (departamento ==="choco") {
+    capital.innerHTML = message + " Quibdó." + message2 + "<a href='http://www.quibdo-choco.gov.co/Paginas/default.aspx'> Quibdó </a>";
 }
-if (departamento ==="cordoba") {
-    capital.innerHTML = message + " Montería." + message2 + "<a href='#'> Montería </a>";
+else if (departamento ==="cordoba") {
+    capital.innerHTML = message + " Montería." + message2 + "<a href='https://www.monteria.gov.co/'> Montería </a>";
 }
-if (departamento ==="cundinamarca") {
-    capital.innerHTML = message + " Bogotá." + message2 + "<a href='#'> Bogotá </a>";
+else if (departamento ==="cundinamarca") {
+    capital.innerHTML = message + " Bogotá." + message2 + "<a href='https://bogota.gov.co/'> Bogotá </a>";
 }
-if (departamento ==="guainia") {
-    capital.innerHTML = message + " Inírida." + message2 + "<a href='#'> Inírida </a>";
+else if (departamento ==="guainia") {
+    capital.innerHTML = message + " Inírida." + message2 + "<a href='http://www.inirida-guainia.gov.co/Paginas/default.aspx'> Inírida </a>";
 }
-if (departamento ==="guaviare") {
-    capital.innerHTML = message + " San José del Guaviare." + message2 + "<a href='#'> San José del Guaviare </a>";
+else if (departamento ==="guaviare") {
+    capital.innerHTML = message + " San José del Guaviare." + message2 + "<a href='https://www.sanjosedelguaviare-guaviare.gov.co/Paginas/default.aspx'> San José del Guaviare </a>";
 }
-if (departamento ==="huila") {
-    capital.innerHTML = message + " Neiva" + message2 + "<a href='#'> Neiva </a>";
+else if (departamento ==="huila") {
+    capital.innerHTML = message + " Neiva" + message2 + "<a href='https://www.alcaldianeiva.gov.co/Paginas/Inicio.aspx'> Neiva </a>";
 }
-if (departamento ==="la guajira") {
-    capital.innerHTML = message + " Riohacha." + message2 + "<a href='#'> Riohacha </a>";
+else if (departamento ==="la guajira") {
+    capital.innerHTML = message + " Riohacha." + message2 + "<a href='https://www.riohacha-laguajira.gov.co/Paginas/Inicio.aspx'> Riohacha </a>";
 }
-if (departamento ==="magdalena") {
-    capital.innerHTML = message + " Santa Marta." + message2 + "<a href='#'> Santa Marta </a>";
+else if (departamento ==="magdalena") {
+    capital.innerHTML = message + " Santa Marta." + message2 + "<a href='https://www.santamarta.gov.co/'> Santa Marta </a>";
 }
-if (departamento ==="meta") {
-    capital.innerHTML = message + " Villavicencio." + message2 + "<a href='#'> Villavicencio </a>";
+else if (departamento ==="meta") {
+    capital.innerHTML = message + " Villavicencio." + message2 + "<a href='https://www.villavicencio.gov.co/'> Villavicencio </a>";
 }
-if (departamento ==="nariño") {
-    capital.innerHTML = message + " San Juan de Pasto." + message2 + "<a href='#'> San Juan de Pasto </a>";
+else if (departamento ==="nariño") {
+    capital.innerHTML = message + " San Juan de Pasto." + message2 + "<a href='https://www.pasto.gov.co/'> San Juan de Pasto </a>";
 }
-if (departamento ==="norte de santander") {
-    capital.innerHTML = message + " San José de Cúcuta" + message2 + "<a href='#'> San José de Cúcuta </a>";
+else if (departamento ==="norte de santander") {
+    capital.innerHTML = message + " San José de Cúcuta" + message2 + "<a href='http://www.cucuta-nortedesantander.gov.co/'> San José de Cúcuta </a>";
 }
-if (departamento ==="putumayo") {
-    capital.innerHTML = message + " Mocoa." + message2 + "<a href='#'> Mocoa </a>";
+else if (departamento ==="putumayo") {
+    capital.innerHTML = message + " Mocoa." + message2 + "<a href='https://www.mocoa-putumayo.gov.co/Paginas/default.aspx'> Mocoa </a>";
 }
-if (departamento ==="quindio") {
-    capital.innerHTML = message + " Armenia." + message2 + "<a href='#'> Armenia </a>";
+else if (departamento ==="quindio") {
+    capital.innerHTML = message + " Armenia." + message2 + "<a href='https://www.armenia.gov.co/'> Armenia </a>";
 }
-if (departamento ==="risaralda") {
-    capital.innerHTML = message + " Pereira." + message2 + "<a href='#'> Pereira </a>";
+else if (departamento ==="risaralda") {
+    capital.innerHTML = message + " Pereira." + message2 + "<a href='http://www.pereira.gov.co/Paginas/Default.aspx'> Pereira </a>";
 }
-if (departamento ==="san andres y providencia") {
-    capital.innerHTML = message + " San Andrés." + message2 + "<a href='#'> San Andrés </a>";
+else if (departamento ==="san andres y providencia") {
+    capital.innerHTML = message + " San Andrés." + message2 + "<a href='https://www.sanandres.gov.co/'> San Andrés </a>";
 }
-if (departamento ==="santander") {
-    capital.innerHTML = message + " Bucaramanga." + message2 + "<a href='#'> Bucaramanga </a>";
+else if (departamento ==="santander") {
+    capital.innerHTML = message + " Bucaramanga." + message2 + "<a href='https://www.bucaramanga.gov.co/Inicio/'> Bucaramanga </a>";
 }
-if (departamento ==="sucre") {
-    capital.innerHTML = message + " Sincelejo." + message2 + "<a href='#'> Sincelejo </a>";
+else if (departamento ==="sucre") {
+    capital.innerHTML = message + " Sincelejo." + message2 + "<a href='https://www.alcaldiadesincelejo.gov.co/Paginas/default.aspx'> Sincelejo </a>";
 }
-if (departamento ==="tolima") {
-    capital.innerHTML = message + " Ibagué." + message2 + "<a href='#'> Bucaramanga </a>";
+else if (departamento ==="tolima") {
+    capital.innerHTML = message + " Ibagué." + message2 + "<a href='https://www.ibague.gov.co'> Ibagué </a>";
 }
-if (departamento ==="valle del cauca") {
-    capital.innerHTML = message + " Cali." + message2 + "<a href='#'> Cali </a>";
-}
-
-if (departamento ==="vaupes") {
-    capital.innerHTML = message + " Mitú." + message2 + "<a href='#'> Mitú </a>";
+else if (departamento ==="valle del cauca") {
+    capital.innerHTML = message + " Cali." + message2 + "<a href='https://www.cali.gov.co/'> Cali </a>";
 }
 
-if (departamento ==="vichada") {
-    capital.innerHTML = message + " Puerto Carreño." + message2 + "<a href='#'> Puerto Carreño </a>";
+else if (departamento ==="vaupes") {
+    capital.innerHTML = message + " Mitú." + message2 + "<a href='https://www.mitu-vaupes.gov.co/Paginas/default.aspx'> Mitú </a>";
+}
+
+else if (departamento ==="vichada") {
+    capital.innerHTML = message + " Puerto Carreño." + message2 + "<a href='https://www.puertocarreno-vichada.gov.co/Paginas/default.aspx'> Puerto Carreño </a>";
 }
 
 else {
